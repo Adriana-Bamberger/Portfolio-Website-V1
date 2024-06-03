bac571d826ab987028bc6d75acdd2e78814f7c1c
Adriana Bamberger <adriana@bamberger.co.nz>
2024-06-03 17:50:21 +1200

    added header details

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,10 +1,7 @@
+import HeaderBar from './HeaderBar'
+//This rendors my mainpage.
 function App() {
-  return (
-    <div>
-      <h1>App</h1>
-      <p>React development has begun!</p>
-    </div>
-  )
+  return <HeaderBar />
 }
 
 export default App
---
+++ client/components/HeaderBar.tsx
@@ -0,0 +1,19 @@
+//The header bar that has my title and buttons
+function HeaderBar() {
+  return (
+    <>
+      <span>Adriana Bamberger | Portfolio</span>
+      <span>
+        <button> Exprience </button>
+      </span>
+      <span>
+        <button> Projects </button>
+      </span>
+      <span>
+        <button> Other Things </button>
+      </span>
+    </>
+  )
+}
+
+export default HeaderBar
