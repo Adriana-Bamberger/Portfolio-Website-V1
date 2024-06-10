fc53e031a5b0dc416d75d1f8cb7be3af85973a25
Adriana Bamberger <adriana@bamberger.co.nz>
2024-06-10 17:18:10 +1200

    got up the images and projects box

--- client/components/ProjectsBox.tsx
+++ client/components/ProjectsBox.tsx
@@ -1,12 +1,35 @@
+// Importing images
+import project1Img from '../Images/Portfolio-on-github.png'
 // A box that shows scrolling images of the projects I've worked on before
 function ProjectsBox() {
   return (
     <>
-    <div className="dark-box">
-      <h1>Projects</h1>
-      <div>
-        <p>boxes will go here</p>
-      </div>
+      <div className="dark-box">
+        <h1>Projects</h1>
+        <div>
+          <div className="project-box">
+            <img
+              src={project1Img}
+              className="project-img"
+              alt="The github repo of this portfolio"
+            />
+            <img
+              src={project1Img}
+              className="project-img"
+              alt="The github repo of this portfolio"
+            />
+            <img
+              src={project1Img}
+              className="project-img"
+              alt="The github repo of this portfolio"
+            />
+            <img
+              src={project1Img}
+              className="project-img"
+              alt="The github repo of this portfolio"
+            />
+          </div>
+        </div>
       </div>
     </>
   )
