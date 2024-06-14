48b788a1209eeffdf7a570c6ed3e1c693cde738a
Adriana Bamberger <adriana@bamberger.co.nz>
2024-06-14 16:33:51 +1200

    imported icons

--- client/components/HeroBox.tsx
+++ client/components/HeroBox.tsx
@@ -1,11 +1,12 @@
 // a box with a opening statment, my img and quick link buttons
 import img from '../Images/Adrib_logo.png'
+import github from
 function HeroBox() {
   return (
     <>
-      <div className="box-to-glow glow hero-box">
-        <div>
-          <h1>Heya!</h1>
+      <div className="dark-box-to-glow glow">
+      <h1>Heya!</h1>
+        <div className='hero-box'>
           <p>
             A bunch of text will go here, We're talking only a sentance about
             this long, It's just so that people can get an idea of what I have
@@ -14,9 +15,16 @@ function HeroBox() {
             reading it right away. <br />I might even put some buttons below so
             that you can navitage to socials...
           </p>
-        </div>
-
         <img src={img} alt="cartoon drawing of adriana by adriana" />
+      </div>
+      {/* Social media icons */}
+      <div class="social-icon social-icon--facebook">
+  <img class="social-icon__image" src="path/to/facebook-bw.png" alt="Facebook Icon (Black and White)">
+</div>
+<div class="social-icon social-icon--twitter">
+  <img class="social-icon__image" src="path/to/twitter-bw.png" alt="Twitter Icon (Black and White)">
+</div>
+
       </div>
     </>
   )
