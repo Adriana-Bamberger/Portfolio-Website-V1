c626e5f56074260d353be8f9d4a849fc2881e5d7
Adriana Bamberger <adriana@bamberger.co.nz>
2024-06-23 11:36:18 +1200

    fixed image imports for better loding time

--- client/components/HeroBox.tsx
+++ client/components/HeroBox.tsx
@@ -1,5 +1,11 @@
 // a box with a opening statment, my img and quick link buttons
 import img from '../Images/Adrib_logo.png'
+import github from '../Images/github-logo.png'
+import Linkedin from '../Images/linkedin-black.png'
+import Instagram from '../Images/instagram-black.png'
+import Gmail from '../Images/gmail-black.png'
+import StackOverflow from '../Images/stack-overflow-black.png'
+import GoodReads from '../Images/goodReads-black.png'
 function HeroBox() {
   return (
     <>
@@ -7,54 +13,54 @@ function HeroBox() {
         <h1>Heya!</h1>
         <div className="hero-box">
           <p>
-            A bunch of text will go here, We're talking only a sentance about
-            this long, It's just so that people can get an idea of what I have
-            to offer, It should have something funny included its like the
-            opening phrase in a book its gotta catch the eye of the person
-            reading it right away. <br />I might even put some buttons below so
-            that you can navitage to socials...
+            A bunch of text will go here, Were talking only a sentance about
+            this long, Its just so that people can get an idea of what I have to
+            offer, It should have something funny included its like the opening
+            phrase in a book its gotta catch the eye of the person reading it
+            right away. <br />I might even put some buttons below so that you
+            can navitage to socials...
           </p>
           <img src={img} alt="cartoon drawing of adriana by adriana" />
         </div>
         <div className="social-icon social-icon--github">
           <img
             className="social-icon__image"
-            src="client/Images/github-logo.png"
+            src={github}
             alt="GitHub Icon (Black and White)"
           />
         </div>
         <div className="social-icon social-icon--linkedIn">
           <img
             className="social-icon__image"
-            src="client/Images/linkedin-black.png"
+            src={Linkedin}
             alt="Linked In Icon (Black and White)"
           />
         </div>
         <div className="social-icon social-icon--instagram">
           <img
             className="social-icon__image"
-            src="client/Images/instagram-black.png"
+            src={Instagram}
             alt="Instagram Icon (Black and White)"
           />
         </div>
         <div className="social-icon social-icon--gmail">
           <img
             className="social-icon__image"
-            src="client/Images/gmail-black.png"
+            src={Gmail}
             alt="Gmail Icon (Black and White)"
           />
         </div>
         <div className="social-icon social-icon--stackOverflow">
           <img
             className="social-icon__image"
-            src="client/Images/stack-overflow-black.png"
+            src={StackOverflow}
             alt="Stack Overflow Icon (Black and White)"
           />
         </div>
         <div className="social-icon social-icon--goodReads">
           <img
             className="social-icon__image"
-            src="client/Images/goodReads-black.png"
+            src={GoodReads}
             alt="Good Reads Icon (Black and White)"
           />
         </div>
