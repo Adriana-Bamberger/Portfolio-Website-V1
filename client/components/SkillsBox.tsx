8f15ee0d56e08115736e4d0c1fb18f20919672a5
Adriana Bamberger <adriana@bamberger.co.nz>
2024-06-25 09:45:34 +1200

    added the list of soft skill programs I can use so far

--- client/components/SkillsBox.tsx
+++ client/components/SkillsBox.tsx
@@ -5,14 +5,34 @@ function SkillsBox() {
       <div className="dark-box">
         <h1>Skills</h1>
         <ul>
-          <li>a soft skill</li>
-          <li> another soft skill</li>
-          <li>and so on with more skills</li>
+          <li>GitHub</li>
+          <li>Clip Studio Paint</li>
+          <li>SketchBook</li>
+          <li>Art Rage</li>
+          <li>MediBang Paint</li>
+          <li>React</li>
+          <li>Visual Studio</li>
+          <li>Express.js</li>
+          <li>Node.js</li>
+          <li>Tailwind CSS</li>
+          <li>Sass</li>
+          <li>CSS</li>
+          <li>HTML</li>
+          <li>Typescript</li>
+          <li>JavaScript</li>
+          <li>Docker</li>
+          <li>Knex</li>
+          <li>Sqlite</li>
+          <li>Ubuntu</li>
+          <li>React Native</li>
+          <li>Expo</li>
+          <li>Firebase</li>
+          <li>Google Workspace</li>
+          <li>Gemini</li>
+          <li>ChatGPT</li>
+          <li>Canva</li>
+          <li>Figma</li>
         </ul>
-        <p>
-          lot of little Images will go here too for the diffrent software and
-          hardwares.
-        </p>
       </div>
     </>
   )
