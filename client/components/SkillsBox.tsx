d2826089e208f4e579b6d445e0335c17af555f86
Adriana Bamberger <adriana@bamberger.co.nz>
2024-06-30 18:22:32 +1200

    added new images

--- client/components/SkillsBox.tsx
+++ client/components/SkillsBox.tsx
@@ -1,40 +1,82 @@
-// A box for me to list out all the skills hard and soft that I have.
+// A box react component for me to list out all the skills hard and soft that I have.
+// Imports to be added soon
 function SkillsBox() {
   return (
-    <>
-      <div className="dark-box">
-        <h1>Skills</h1>
-        <ul>
-          <li>GitHub</li>
-          <li>Clip Studio Paint</li>
-          <li>SketchBook</li>
-          <li>Art Rage</li>
-          <li>MediBang Paint</li>
-          <li>React</li>
-          <li>Visual Studio</li>
-          <li>Express.js</li>
-          <li>Node.js</li>
-          <li>Tailwind CSS</li>
-          <li>Sass</li>
-          <li>CSS</li>
-          <li>HTML</li>
-          <li>Typescript</li>
-          <li>JavaScript</li>
-          <li>Docker</li>
-          <li>Knex</li>
-          <li>Sqlite</li>
-          <li>Ubuntu</li>
-          <li>React Native</li>
-          <li>Expo</li>
-          <li>Firebase</li>
-          <li>Google Workspace</li>
-          <li>Gemini</li>
-          <li>ChatGPT</li>
-          <li>Canva</li>
-          <li>Figma</li>
-        </ul>
+    <div className="dark-box">
+      <h1>Skills</h1>
+      <ul>
+        <li>GitHub</li>
+        <li>Clip Studio Paint</li>
+        <li>SketchBook</li>
+        <li>Art Rage</li>
+        <li>MediBang Paint</li>
+        <li>React</li>
+        <li>Visual Studio</li>
+        <li>Express.js</li>
+        <li>Node.js</li>
+        <li>Tailwind CSS</li>
+        <li>Sass</li>
+        <li>CSS</li>
+        <li>HTML</li>
+        <li>Typescript</li>
+        <li>JavaScript</li>
+        <li>Docker</li>
+        <li>Knex</li>
+        <li>Sqlite</li>
+        <li>Ubuntu</li>
+        <li>React Native</li>
+        <li>Expo</li>
+        <li>Firebase</li>
+        <li>Google Workspace</li>
+        <li>Gemini</li>
+        <li>ChatGPT</li>
+        <li>Canva</li>
+        <li>Figma</li>
+      </ul>
+      <div className="scroll textScrollSpeed">
+        <div>
+          <span>HTML</span>
+          <span>CSS</span>
+          <span>JavaScript</span>
+          <span>ReactJS</span>
+          <span>Figma</span>
+          <span>Photoshop</span>
+          <span>Premiere Pro</span>
+          <span>Figma</span>
+          <span>Angular</span>
+          <span>Node JS</span>
+        </div>
+        {/* <div className="scroll imgBox imgScrollSpeed">
+          <div>
+            <img src={github} alt="" />
+            <img src={clipStudioPaint} alt="" />
+            <img src={sketchBook} alt="" />
+            <img src={artRage} alt="" />
+            <img src={mediBangPaint} alt="" />
+            <img src={react} alt="" />
+            <img src={visualStudio} alt="" />
+            <img src={expressJs} alt="" />
+            <img src={nodeJs} alt="" />
+            <img src={tailwind} alt="" />
+            <img src={Sass} alt="" />
+            <img src={html} alt="" />
+            <img src={typeScript} alt="" />
+            <img src={docker} alt="" />
+            <img src={knex} alt="" />
+            <img src={sqlite} alt="" />
+            <img src={ubuntu} alt="" />
+            <img src={reactNative} alt="" />
+            <img src={expo} alt="" />
+            <img src={firebase} alt="" />
+            <img src={googleWorkspace} alt="" />
+            <img src={gemini} alt="" />
+            <img src={chatGpt} alt="" />
+            <img src={canva} alt="" />
+            <img src={figma} alt="" />
+          </div>
+        </div> */}
       </div>
-    </>
+    </div>
   )
 }
 export default SkillsBox
