6b8c07589ac025c5038535ed74183c5b7342c0c1
Adriana Bamberger <adriana@bamberger.co.nz>
2024-07-03 18:05:47 +1200

    added a cv to by cv button

--- client/components/HeaderBar.tsx
+++ client/components/HeaderBar.tsx
@@ -1,5 +1,7 @@
 //The header bar that has my title and buttons
 function HeaderBar() {
+  const cvDownloadUrl =
+    'https://drive.google.com/file/d/1zx22_dCGr4YnZ6O81ZJzWKJy1ny3yUa4/view?usp=sharing'
   return (
     <>
       <div className="header">
@@ -8,7 +10,9 @@ function HeaderBar() {
           {' '}
           <button className="button">Experience</button>
           <button className="button">Projects</button>
-          <button className="button">Download CV</button>
+          <a href={cvDownloadUrl} className="button">
+            Download CV
+          </a>
         </div>
       </div>
     </>
