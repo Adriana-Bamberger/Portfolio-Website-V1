ddaa7942561e14d571d18126d3d6de6c476eae47
Adriana Bamberger <adriana@bamberger.co.nz>
2024-07-04 10:29:23 +1200

    adding last nights work

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,20 +1,37 @@
 //My footer with all the inport contact deatils on it.
+//Import Images
+import Insta from '../Images/instagram.png'
+import Messenger from '../Images/messenger.png'
+import linkedIn from '../Images/linkedin.png'
+import weChat from '../Images/wechat.png'
 function Footer() {
   return (
     <>
-      <div className="white-box">
-        <span>
-          <p>IMG</p>
-        </span>
-        <span>
-          <h2>
-            Adriana Bamberger | Auckland, New Zealand | adriana@bamberger.nz
-          </h2>
-        </span>
-        <span>Messenger IMG </span>
-        <span>Insta IMG </span>
-        <span>Linked-In IMG </span>
-        <span>We-Chat IMG </span>
+      <div className="white-box footer">
+        <img
+          className="social-icon__image"
+          src={Messenger}
+          alt="Messenger Logo Button"
+        />
+        <img
+          className="social-icon__image"
+          src={Insta}
+          alt="Insta Logo Button"
+        />
+        <h3>
+          Tāmaki Makaurau, Aotearoa | New Zealand Citizen | adriana@bamberger.nz
+          | 022 470 6342
+        </h3>
+        <img
+          className="social-icon__image"
+          src={linkedIn}
+          alt="LinkedIn Logo Button"
+        />
+        <img
+          className="social-icon__image"
+          src={weChat}
+          alt="We Chat Logo Button"
+        />
       </div>
     </>
   )
