658fa08da3d82295c3a65af61643f07b2b5deef4
Adriana Bamberger <adriana@bamberger.co.nz>
2024-07-04 11:54:25 +1200

    moving header and hero layout

--- client/components/HeaderBar.tsx
+++ client/components/HeaderBar.tsx
@@ -1,18 +1,66 @@
 //The header bar that has my title and buttons
+//Importing the Button images.
+import github from '../Images/github-logo.png'
+import Linkedin from '../Images/linkedin-black.png'
+import Instagram from '../Images/instagram-black.png'
+import Gmail from '../Images/gmail-black.png'
+import StackOverflow from '../Images/stack-overflow-black.png'
+import GoodReads from '../Images/goodReads-black.png'
 function HeaderBar() {
   const cvDownloadUrl =
     'https://drive.google.com/file/d/1zx22_dCGr4YnZ6O81ZJzWKJy1ny3yUa4/view?usp=sharing'
   return (
     <>
       <div className="header">
-        <span className="site-header">Adriana Bamberger | Portfolio</span>
-        <div className="buttons-container">
-          {' '}
-          <button className="button">Experience</button>
-          <button className="button">Projects</button>
-          <a href={cvDownloadUrl} className="button">
-            Download CV
-          </a>
+        <span className="header-title">Adriana Bamberger | Portfolio</span>
+        <div>
+          <div className="buttons-container">
+            <div className="social-icon social-icon--github">
+              <img
+                className="social-icon__image"
+                src={github}
+                alt="GitHub Icon (Black and White)"
+              />
+            </div>
+            <div className="social-icon social-icon--linkedIn">
+              <img
+                className="social-icon__image"
+                src={Linkedin}
+                alt="Linked In Icon (Black and White)"
+              />
+            </div>
+            <div className="social-icon social-icon--instagram">
+              <img
+                className="social-icon__image"
+                src={Instagram}
+                alt="Instagram Icon (Black and White)"
+              />
+            </div>
+            <div className="social-icon social-icon--gmail">
+              <img
+                className="social-icon__image"
+                src={Gmail}
+                alt="Gmail Icon (Black and White)"
+              />
+            </div>
+            <div className="social-icon social-icon--stackOverflow">
+              <img
+                className="social-icon__image"
+                src={StackOverflow}
+                alt="Stack Overflow Icon (Black and White)"
+              />
+            </div>
+            <div className="social-icon social-icon--goodReads">
+              <img
+                className="social-icon__image"
+                src={GoodReads}
+                alt="Good Reads Icon (Black and White)"
+              />
+            </div>
+            {/* <a href={cvDownloadUrl} className="button">
+              Download CV
+            </a> */}
+          </div>
         </div>
       </div>
     </>
--- client/components/HeroBox.tsx
+++ client/components/HeroBox.tsx
@@ -1,18 +1,12 @@
 // a box with a opening statment, my img and quick link buttons
 import img from '../Images/Adrib_logo.png'
-import github from '../Images/github-logo.png'
-import Linkedin from '../Images/linkedin-black.png'
-import Instagram from '../Images/instagram-black.png'
-import Gmail from '../Images/gmail-black.png'
-import StackOverflow from '../Images/stack-overflow-black.png'
-import GoodReads from '../Images/goodReads-black.png'
+
 function HeroBox() {
   return (
     <>
       <div className="dark-box-to-glow glow">
-        <h1>Heya!</h1>
         <p className="subheading">
-          Kia ora! I'm a full-stack developer with a passion for creative
+          <b>Kia ora! </b>I'm a full-stack developer with a passion for creative
           problem-solving.{' '}
         </p>
         <div className="hero-box">
@@ -32,48 +26,6 @@ function HeroBox() {
             environments that embrace exploration and new challenges.
           </p>
           <img src={img} alt="cartoon drawing of adriana by adriana" />
-        </div>
-        <div className="social-icon social-icon--github">
-          <img
-            className="social-icon__image"
-            src={github}
-            alt="GitHub Icon (Black and White)"
-          />
-        </div>
-        <div className="social-icon social-icon--linkedIn">
-          <img
-            className="social-icon__image"
-            src={Linkedin}
-            alt="Linked In Icon (Black and White)"
-          />
-        </div>
-        <div className="social-icon social-icon--instagram">
-          <img
-            className="social-icon__image"
-            src={Instagram}
-            alt="Instagram Icon (Black and White)"
-          />
-        </div>
-        <div className="social-icon social-icon--gmail">
-          <img
-            className="social-icon__image"
-            src={Gmail}
-            alt="Gmail Icon (Black and White)"
-          />
-        </div>
-        <div className="social-icon social-icon--stackOverflow">
-          <img
-            className="social-icon__image"
-            src={StackOverflow}
-            alt="Stack Overflow Icon (Black and White)"
-          />
-        </div>
-        <div className="social-icon social-icon--goodReads">
-          <img
-            className="social-icon__image"
-            src={GoodReads}
-            alt="Good Reads Icon (Black and White)"
-          />
         </div>
       </div>
     </>
