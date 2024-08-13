5165e867cb81f71359b89b16290027eef0d65c3b
Adriana Bamberger <adriana@bamberger.co.nz>
2024-08-13 22:09:14 +1200

    image spacing and z levels added

--- client/components/HeroBox.tsx
+++ client/components/HeroBox.tsx
@@ -4,25 +4,23 @@ import img from '../Images/Advertars/3D-Advtar.png'
 function HeroBox() {
   return (
     <>
-      <div className="box">
+      <div className="box hero-box">
         <p className="subheading">
           <b>Kia ora! </b>I&apos;m a Kiwi full-stack developer in Auckland who
-          loves blending creativity with technology.{' '}
+          loves blending creativity with technology. <br />
+          I started coding at twelve and honed my skills at Dev Academy
+          Aotearoa.
+          <br />
+          <br />
+          My background in costume creation, retail, and Scouting has given me
+          strong collaboration and communication skills. <br />
+          I love supportive, collaborative environments and am always eager to
+          learn and grow. <br />
+          Check out my projects below, or send me a message. Let&apos;s build
+          something great together!
         </p>
-        <div className="hero-box">
-          <p>
-            I started coding at twelve and honed my skills at Dev Academy
-            Aotearoa.
-            <br />
-            My background in costume creation, retail, and Scouting has given me
-            strong collaboration and communication skills. <br />
-            I love supportive, collaborative environments and am always eager to
-            learn and grow. <br />
-            Check out my projects below, or send me a message. Let&apos;s build
-            something great together!
-          </p>
-          <img src={img} alt="cartoon drawing of adriana by adriana" />
-        </div>
+        <img src={img} alt="cartoon drawing of adriana by adriana" />
+        {/* </div> */}
       </div>
     </>
   )
