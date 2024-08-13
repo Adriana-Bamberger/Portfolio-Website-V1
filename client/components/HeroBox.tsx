d8321cf7eacf4ceefb7b5996a11a7dd41f51cec5
Adriana Bamberger <adriana@bamberger.co.nz>
2024-08-13 21:06:15 +1200

    new images:

--- client/components/HeroBox.tsx
+++ client/components/HeroBox.tsx
@@ -1,13 +1,13 @@
 // a box with a opening statment, my img and quick link buttons
-import img from '../Images/Adrib_logo.png'
+import img from '../Images/Advertars/3D-Advtar.png'
 
 function HeroBox() {
   return (
     <>
       <div className="box">
         <p className="subheading">
-          <b>Kia ora! </b>I'm a Kiwi full-stack developer in Auckland who loves
-          blending creativity with technology.{' '}
+          <b>Kia ora! </b>I&apos;m a Kiwi full-stack developer in Auckland who
+          loves blending creativity with technology.{' '}
         </p>
         <div className="hero-box">
           <p>
@@ -18,7 +18,7 @@ function HeroBox() {
             strong collaboration and communication skills. <br />
             I love supportive, collaborative environments and am always eager to
             learn and grow. <br />
-            Check out my projects below, or send me a message. Let's build
+            Check out my projects below, or send me a message. Let&apos;s build
             something great together!
           </p>
           <img src={img} alt="cartoon drawing of adriana by adriana" />
