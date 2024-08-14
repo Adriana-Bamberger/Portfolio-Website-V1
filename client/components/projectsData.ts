dc0ae934119256e69600be79cc1a6549baab5fad
Adriana Bamberger <adriana@bamberger.co.nz>
2024-08-14 16:17:25 +1200

    small update lol

--- client/components/projectsData.ts
+++ client/components/projectsData.ts
@@ -2,15 +2,27 @@
 export const projects = [
   {
     id: '1',
-    title: 'Project One',
+    title: 'WedPro',
     description: 'Details about Project One.',
-    image: 'path/to/image1.png',
+    image: 'client/Images/Projects/WedPro.svg',
   },
   {
     id: '2',
-    title: 'Project Two',
+    title: 'OOTD',
     description: 'Details about Project Two.',
-    image: 'path/to/image2.png',
+    image: 'client/Images/Projects/OOTD.svg',
+  },
+  {
+    id: '3',
+    title: 'Dev Academy',
+    description: 'Details about Project Three.',
+    image: 'client/Images/Projects/Dev-Academy.svg',
+  },
+  {
+    id: '4',
+    title: 'Portfolio',
+    description: 'Details about Project Four.',
+    image: 'client/Images/Projects/Portfolio-needs-update.svg',
   },
   // Add more projects as needed
 ]
