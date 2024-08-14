f1c391e85b9c5bc115760a7c104fb45d4eec170e
Adriana Bamberger <adriana@bamberger.co.nz>
2024-08-14 16:58:20 +1200

    new svgs and animated backgorund implmented

--- client/components/projectsData.ts
+++ client/components/projectsData.ts
@@ -4,25 +4,25 @@ export const projects = [
     id: '1',
     title: 'WedPro',
     description: 'Details about Project One.',
-    image: 'client/Images/Projects/WedPro.svg',
+    image: 'client/Images/Projects/WedPro-Claer.svg',
   },
   {
     id: '2',
     title: 'OOTD',
     description: 'Details about Project Two.',
-    image: 'client/Images/Projects/OOTD.svg',
+    image: 'client/Images/Projects/OOTD-Clear.svg',
   },
   {
     id: '3',
     title: 'Dev Academy',
     description: 'Details about Project Three.',
-    image: 'client/Images/Projects/Dev-Academy.svg',
+    image: 'client/Images/Projects/Dev-Academy-Clear.svg',
   },
   {
     id: '4',
     title: 'Portfolio',
     description: 'Details about Project Four.',
-    image: 'client/Images/Projects/Portfolio-needs-update.svg',
+    image: 'client/Images/Projects/Portfolio-Clear.svg',
   },
   // Add more projects as needed
 ]
