1ae88299e604be4ea0324a84cdb2d3d2a8ee90d6
Adriana Bamberger <adriana@bamberger.co.nz>
2024-08-19 15:10:10 +1200

    swaped svgs out for pngs in attempt to fix load time

--- client/components/projectsData.ts
+++ client/components/projectsData.ts
@@ -4,25 +4,25 @@ export const projects = [
     id: '1',
     title: 'WedPro',
     description: 'Details about Project One.',
-    image: 'client/Images/Projects/WedPro-Claer.svg',
+    image: 'client/Images/Projects/Wedpro-Clear.png',
   },
   {
     id: '2',
     title: 'OOTD',
     description: 'Details about Project Two.',
-    image: 'client/Images/Projects/OOTD-Clear.svg',
+    image: 'client/Images/Projects/OOTD-Clear.png',
   },
   {
     id: '3',
     title: 'Dev Academy',
     description: 'Details about Project Three.',
-    image: 'client/Images/Projects/Dev-Academy-Clear.svg',
+    image: 'client/Images/Projects/DevAcademy-Clear.png',
   },
   {
     id: '4',
     title: 'Portfolio',
     description: 'Details about Project Four.',
-    image: 'client/Images/Projects/Portfolio-Clear.svg',
+    image: 'client/Images/Projects/Portfolio-Clear.png',
   },
   // Add more projects as needed
 ]
