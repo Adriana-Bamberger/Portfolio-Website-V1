cf3390713116f334b2393af061a0456313fb6eab
Adriana Bamberger <adriana@bamberger.co.nz>
2024-08-20 23:11:17 +1200

    refactor footer code to match

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -1,49 +1,60 @@
-// Currently not in separate components as per best prastice, but can be refactored to be in separate components later if needed.
-import React from 'react'
+import SocialIcon from './SocialIcon'
 import Insta from '../Images/Social Icons/instagram.png'
 import LinkedIn from '../Images/Social Icons/linkedin.png'
 import WeChat from '../Images/Social Icons/wechat.png'
 import Github from '../Images/Social Icons/github-logo.png'
 
-interface SocialIconProps {
-  href: string
-  src: string
-  alt: string
+const Footer: React.FC = () => {
+  const socialLinks = [
+    {
+      href: 'https://github.com/Adriana-Bamberger',
+      src: Github,
+      alt: 'Github Logo Button',
+    },
+    {
+      href: 'https://nz.linkedin.com/in/adriana-bamberger-a58742205',
+      src: LinkedIn,
+      alt: 'LinkedIn Logo Button',
+    },
+    {
+      href: 'https://www.instagram.com/adri.bamberger/?hl=en',
+      src: Insta,
+      alt: 'Insta Logo Button',
+    },
+    {
+      href: 'https://wechat.com/adriana-bamberger',
+      src: WeChat,
+      alt: 'WeChat Logo Button',
+    },
+  ]
+
+  const firstTwoLinks = socialLinks.slice(0, 2)
+  const lastTwoLinks = socialLinks.slice(2)
+
+  return (
+    <div className="box footer">
+      {firstTwoLinks.map((link) => (
+        <SocialIcon
+          key={link.href}
+          href={link.href}
+          src={link.src}
+          alt={link.alt}
+        />
+      ))}
+      <h3>
+        Tāmaki Makaurau, Aotearoa | New Zealand Citizen | adriana@bamberger.nz |
+        022 470 6342
+      </h3>
+      {lastTwoLinks.map((link) => (
+        <SocialIcon
+          key={link.href}
+          href={link.href}
+          src={link.src}
+          alt={link.alt}
+        />
+      ))}
+    </div>
+  )
 }
-
-const SocialIcon: React.FC<SocialIconProps> = ({ href, src, alt }) => (
-  <a href={href} target="_blank" rel="noopener noreferrer">
-    <img className="social-icon__image" src={src} alt={alt} loading="lazy" />
-  </a>
-)
-
-const Footer: React.FC = () => (
-  <div className="box footer">
-    <SocialIcon
-      href="https://github.com/Adriana-Bamberger"
-      src={Github}
-      alt="Github Logo Button"
-    />
-    <SocialIcon
-      href="https://nz.linkedin.com/in/adriana-bamberger-a58742205"
-      src={LinkedIn}
-      alt="LinkedIn Logo Button"
-    />
-    <h3>
-      Tāmaki Makaurau, Aotearoa | New Zealand Citizen | adriana@bamberger.nz |
-      022 470 6342
-    </h3>
-    <SocialIcon
-      href="https://www.instagram.com/adri.bamberger/?hl=en"
-      src={Insta}
-      alt="Insta Logo Button"
-    />
-    <SocialIcon
-      href="https://u.wechat.com/kAwZCbgHLMQFM09IJLbae1s?s=1"
-      src={WeChat}
-      alt="We Chat Logo Button"
-    />
-  </div>
-)
 
 export default Footer
