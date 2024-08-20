47a075e9afc90e9f2483d91776b4291fa9e7bb87
Adriana Bamberger <adriana@bamberger.co.nz>
2024-08-20 23:00:37 +1200

    refactor header code

--- client/components/HeaderBar.tsx
+++ client/components/HeaderBar.tsx
@@ -1,3 +1,4 @@
+import SocialIcon from './SocialIcon'
 import Github from '../Images/Social Icons/github-logo.png'
 import Linkedin from '../Images/Social Icons/linkedin-black.png'
 import Instagram from '../Images/Social Icons/instagram-black.png'
@@ -5,66 +6,57 @@ import StackOverflow from '../Images/Social Icons/stack-overflow-black.png'
 import GoodReads from '../Images/Social Icons/goodReads-black.png'
 import DownloadCV from '../Images/Social Icons/CV-Download-Icon-black.png'
 
-const socialLinks = [
-  {
-    href: 'https://github.com/Adriana-Bamberger',
-    src: Github,
-    alt: 'GitHub Icon (Black and White)',
-    className: 'social-icon--github',
-  },
-  {
-    href: 'https://nz.linkedin.com/in/adriana-bamberger-a58742205',
-    src: Linkedin,
-    alt: 'LinkedIn Icon (Black and White)',
-    className: 'social-icon--linkedIn',
-  },
-  {
-    href: 'https://www.instagram.com/adri.bamberger/?hl=en',
-    src: Instagram,
-    alt: 'Instagram Icon (Black and White)',
-    className: 'social-icon--instagram',
-  },
-  {
-    href: 'https://stackoverflow.com/users/23250670/adriana-bamberger',
-    src: StackOverflow,
-    alt: 'Stack Overflow Icon (Black and White)',
-    className: 'social-icon--stackOverflow',
-  },
-  {
-    href: 'https://www.goodreads.com/user/show/166058375-adriana-bamberger',
-    src: GoodReads,
-    alt: 'Good Reads Icon (Black and White)',
-    className: 'social-icon--goodReads',
-  },
-  {
-    href: 'https://drive.google.com/file/d/1iagpXERR6W1qiFfLOncFBi0Ogm_3chTY/view?usp=sharing',
-    src: DownloadCV,
-    alt: 'Download CV',
-    className: 'social-icon--downloadCv',
-  },
-]
-
 function HeaderBar() {
+  const socialLinks = [
+    {
+      href: 'https://github.com/Adriana-Bamberger',
+      src: Github,
+      alt: 'GitHub Icon (Black and White)',
+      className: 'social-icon--github',
+    },
+    {
+      href: 'https://nz.linkedin.com/in/adriana-bamberger-a58742205',
+      src: Linkedin,
+      alt: 'LinkedIn Icon (Black and White)',
+      className: 'social-icon--linkedIn',
+    },
+    {
+      href: 'https://www.instagram.com/adri.bamberger/?hl=en',
+      src: Instagram,
+      alt: 'Instagram Icon (Black and White)',
+      className: 'social-icon--instagram',
+    },
+    {
+      href: 'https://stackoverflow.com/users/23250670/adriana-bamberger',
+      src: StackOverflow,
+      alt: 'Stack Overflow Icon (Black and White)',
+      className: 'social-icon--stackOverflow',
+    },
+    {
+      href: 'https://www.goodreads.com/user/show/166058375-adriana-bamberger',
+      src: GoodReads,
+      alt: 'Good Reads Icon (Black and White)',
+      className: 'social-icon--goodReads',
+    },
+    {
+      href: 'https://drive.google.com/file/d/1iagpXERR6W1qiFfLOncFBi0Ogm_3chTY/view?usp=sharing',
+      src: DownloadCV,
+      alt: 'Download CV',
+      className: 'social-icon--downloadCv',
+    },
+  ]
   return (
     <div className="header">
       <span className="header-title">Adriana Bamberger | Portfolio - WIP </span>
       <div className="buttons-container">
         {socialLinks.map((link) => (
-          <a
+          <SocialIcon
             key={link.href}
             href={link.href}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className={`social-icon ${link.className}`}>
-              <img
-                className="social-icon__image"
-                src={link.src}
-                alt={link.alt}
-                loading="lazy"
-              />
-            </div>
-          </a>
+            src={link.src}
+            alt={link.alt}
+            className={link.className}
+          />
         ))}
       </div>
     </div>
@@ -72,16 +64,3 @@ function HeaderBar() {
 }
 
 export default HeaderBar
-
-{
-  /* Gmail is very slow to do the link, not sure it's worth having.
-          <a href="mailto:adriana@bamberger.nz">
-            <div className="social-icon social-icon--gmail">
-              <img
-                className="social-icon__image"
-                src={Gmail}
-                alt="Gmail Icon (Black and White)"
-              />
-            </div>
-          </a> */
-}
--- client/components/SocialIcon.tsx
+++ client/components/SocialIcon.tsx
@@ -4,11 +4,19 @@ interface SocialIconProps {
   href: string
   src: string
   alt: string
+  className?: string
 }
 
-const SocialIcon: React.FC<SocialIconProps> = ({ href, src, alt }) => (
+const SocialIcon: React.FC<SocialIconProps> = ({
+  href,
+  src,
+  alt,
+  className,
+}) => (
   <a href={href} target="_blank" rel="noopener noreferrer">
-    <img className="social-icon__image" src={src} alt={alt} loading="lazy" />
+    <div className={`social-icon ${className}`}>
+      <img className="social-icon__image" src={src} alt={alt} loading="lazy" />
+    </div>
   </a>
 )
 
