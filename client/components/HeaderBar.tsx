c086dfdeea41c73eb92895f9fa08b4762dbe0c0f
Adriana Bamberger <adriana@bamberger.co.nz>
2024-09-23 22:58:54 +1200

    temp remove hover styles

--- client/components/HeaderBar.tsx
+++ client/components/HeaderBar.tsx
@@ -12,37 +12,37 @@ function HeaderBar() {
       href: 'https://github.com/Adriana-Bamberger',
       src: Github,
       alt: 'GitHub Icon (Black and White)',
-      className: 'social-icon--github',
+      // className: 'social-icon--github',
     },
     {
       href: 'https://nz.linkedin.com/in/adriana-bamberger-a58742205',
       src: Linkedin,
       alt: 'LinkedIn Icon (Black and White)',
-      className: 'social-icon--linkedIn',
+      // className: 'social-icon--linkedIn',
     },
     {
       href: 'https://www.instagram.com/adri.bamberger/?hl=en',
       src: Instagram,
       alt: 'Instagram Icon (Black and White)',
-      className: 'social-icon--instagram',
+      // className: 'social-icon--instagram',
     },
     {
       href: 'https://stackoverflow.com/users/23250670/adriana-bamberger',
       src: StackOverflow,
       alt: 'Stack Overflow Icon (Black and White)',
-      className: 'social-icon--stackOverflow',
+      // className: 'social-icon--stackOverflow',
     },
     {
       href: 'https://www.goodreads.com/user/show/166058375-adriana-bamberger',
       src: GoodReads,
       alt: 'Good Reads Icon (Black and White)',
-      className: 'social-icon--goodReads',
+      // className: 'social-icon--goodReads',
     },
     {
       href: 'https://drive.google.com/file/d/1W6KXj_fydQK3ZhKZGCfOXkeCd5zt-6te/view?usp=sharing',
       src: DownloadCV,
       alt: 'Download CV',
-      className: 'social-icon--downloadCv',
+      // className: 'social-icon--downloadCv',
     },
   ]
   return (
@@ -57,7 +57,7 @@ function HeaderBar() {
             href={link.href}
             src={link.src}
             alt={link.alt}
-            className={`social-icon ${link.className}`}
+            // className={`social-icon ${link.className}`}
           />
         ))}
       </div>
