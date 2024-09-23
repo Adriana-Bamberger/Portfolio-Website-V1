d8b34c94939a6202ab917c903b74dc360675eba9
Adriana Bamberger <adriana@bamberger.co.nz>
2024-09-23 23:24:41 +1200

    update project imgs

--- client/components/projectsData.ts
+++ client/components/projectsData.ts
@@ -1,28 +1,35 @@
 // projectsData.ts
+import wedProImg from '../Images/Projects/WedPro-Clear.png'
+import ootdImg from '../Images/Projects/OOTD-Clear.png'
+import devAcademyImg from '../Images/Projects/DevAcademy-Clear.png'
+import portfolioImg from '../Images/Projects/Portfolio-Clear.png'
+
 export const projects = [
   {
     id: '1',
     title: 'WedPro',
-    description: 'Details about Project One.',
-    image: 'client/Images/Projects/WedPro-Clear.png',
+    description:
+      'A web application for wedding planning, built with Laravel, Filament and Vue.',
+    image: wedProImg,
   },
   {
     id: '2',
     title: 'OOTD',
-    description: 'Details about Project Two.',
-    image: 'client/Images/Projects/OOTD-Clear.png',
+    description:
+      'A native app for Woodrobe management and sharing made with React Native.',
+    image: ootdImg,
   },
   {
     id: '3',
     title: 'Dev Academy',
-    description: 'Details about Project Three.',
-    image: 'client/Images/Projects/DevAcademy-Clear.png',
+    description:
+      'A Bunch of Projects from the coding bootcamp, built with React, TypeScript and many more.',
+    image: devAcademyImg,
   },
   {
     id: '4',
     title: 'Portfolio',
-    description: 'Details about Project Four.',
-    image: 'client/Images/Projects/Portfolio-Clear.png',
+    description: 'This website, and its many versions.',
+    image: portfolioImg,
   },
-  // Add more projects as needed
 ]
