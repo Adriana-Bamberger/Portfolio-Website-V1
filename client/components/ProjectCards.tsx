3762d051f818db4dc33ad207d4d5ae62ac3a1326
Adriana Bamberger <162661198+Adriana-Bamberger@users.noreply.github.com>
2024-10-21 10:08:46 +1300

    hide navigations I'm not using yet

--- client/components/ProjectCards.tsx
+++ client/components/ProjectCards.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import { useNavigate } from 'react-router-dom'
+// import { useNavigate } from 'react-router-dom'
 import { projects } from './projectsData'
 
 function ProjectsCards() {
