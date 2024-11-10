d74e0a3d9e9ae5e50c3601b01b77809a2a32ecbe
Adriana Bamberger <162661198+Adriana-Bamberger@users.noreply.github.com>
2024-11-11 11:37:56 +1300

    New Header Bar

--- client/components/HeaderBar.tsx
+++ client/components/HeaderBar.tsx
@@ -12,52 +12,47 @@ function HeaderBar() {
       href: 'https://github.com/Adriana-Bamberger',
       src: Github,
       alt: 'GitHub Icon (Black and White)',
-      // className: 'social-icon--github',
     },
     {
       href: 'https://nz.linkedin.com/in/adriana-bamberger-a58742205',
       src: Linkedin,
       alt: 'LinkedIn Icon (Black and White)',
-      // className: 'social-icon--linkedIn',
     },
     {
       href: 'https://www.instagram.com/adri.bamberger/?hl=en',
       src: Instagram,
       alt: 'Instagram Icon (Black and White)',
-      // className: 'social-icon--instagram',
     },
     {
       href: 'https://stackoverflow.com/users/23250670/adriana-bamberger',
       src: StackOverflow,
       alt: 'Stack Overflow Icon (Black and White)',
-      // className: 'social-icon--stackOverflow',
     },
     {
       href: 'https://www.goodreads.com/user/show/166058375-adriana-bamberger',
       src: GoodReads,
       alt: 'Good Reads Icon (Black and White)',
-      // className: 'social-icon--goodReads',
     },
     {
       href: 'https://drive.google.com/file/d/1W6KXj_fydQK3ZhKZGCfOXkeCd5zt-6te/view?usp=sharing',
       src: DownloadCV,
       alt: 'Download CV',
-      // className: 'social-icon--downloadCv',
     },
   ]
   return (
-    <div className="header">
+    <div className="bg-white bg-opacity-10 border border-white border-opacity-0 rounded-custom backdrop-blur-sm mx-10 my-5 p-2.5 flex justify-between items-center md:mx-6 md:my-4 lg:mx-8 lg:my-6 xl:mx-10 xl:my-8 2xl:mx-12 2xl:my-10">
       <span>
-        <h1>Adriana Bamberger | Portfolio</h1>
+        <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl">
+          Adriana Bamberger | Portfolio
+        </h1>
       </span>
-      <div className="buttons-container">
+      <div className="flex gap-2 md:gap-4">
         {socialLinks.map((link) => (
           <SocialIcon
             key={link.href}
             href={link.href}
             src={link.src}
             alt={link.alt}
-            // className={`social-icon ${link.className}`}
           />
         ))}
       </div>
--- client/components/SocialIcon.tsx
+++ client/components/SocialIcon.tsx
@@ -7,15 +7,15 @@ interface SocialIconProps {
   className?: string
 }
 
-const SocialIcon: React.FC<SocialIconProps> = ({
-  href,
-  src,
-  alt,
-  className,
-}) => (
+const SocialIcon: React.FC<SocialIconProps> = ({ href, src, alt }) => (
   <a href={href} target="_blank" rel="noopener noreferrer">
-    <div className={`social-icon ${className}`}>
-      <img className="social-icon__image" src={src} alt={alt} loading="lazy" />
+    <div className="flex items-center justify-center w-11 h-11 mx-1 sm:w-10 sm:h-10">
+      <img
+        className="object-contain w-full h-full"
+        src={src}
+        alt={alt}
+        loading="lazy"
+      />
     </div>
   </a>
 )
