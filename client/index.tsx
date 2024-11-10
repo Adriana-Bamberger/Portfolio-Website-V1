a5be14af704006943cb437a31e85e73131ff49e9
Adriana Bamberger <162661198+Adriana-Bamberger@users.noreply.github.com>
2024-11-11 11:37:26 +1300

    more config

--- client/index.tsx
+++ client/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-
+import './main.css'
 import App from './components/App.tsx'
 
 document.addEventListener('DOMContentLoaded', () => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  content: ['./client/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        'custom': '2rem',
+      },
+      colors: {
+        clr1: '#E40303',
+        clr2: '#FF8C00',
+        clr3: '#FFED00',
+        clr4: '#008026',
+        clr5: '#24408E',
+        clr6: '#732982',
+        clr7: '#414141',
+        clr8: '#B3B3B3',
+        clr9: '#E6E6E6',
+        clr10: '#ffffff',
+        clr11: '#1E1E1E',
+      },
+      backgroundImage: {
+        'background-large':
+          "url('/client/Images/Background/background-large.png')",
+        'background-medium':
+          "url('/client/Images/Background/background-medium.png')",
+        'background-small':
+          "url('/client/Images/Background/background-small.png')",
+        'background-extra-large':
+          "url('/client/Images/Background/background-extra-large.png')",
+      },
+    },
   },
   plugins: [],
 }
