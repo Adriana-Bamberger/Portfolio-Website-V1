e7c894076162126212987243367ffcc71e871572
Adriana Bamberger <162661198+Adriana-Bamberger@users.noreply.github.com>
2025-02-11 21:43:48 +1300

    Add AOS animations package

--- client/index.tsx
+++ client/index.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './main.css'
+import 'aos/dist/aos.css';
 import App from './components/App.tsx'
 
 document.addEventListener('DOMContentLoaded', () => {
